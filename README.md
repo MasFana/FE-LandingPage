d7fbfa988d20fb030ffa3248aefe35f8bc1d9523
Iannn-vbeta <232410103085@mail.unej.ac.id>
2025-03-26 17:16:32 +0700

    Front edn

---
+++ README.md
@@ -0,0 +1,19 @@
+## Deskripsi
+
+Proyek ini adalah sebuah landing page sederhana yang dirancang untuk memberikan informasi tentang suatu produk, layanan, atau tujuan tertentu. Landing page ini dibuat dengan menggunakan teknologi web modern.
+
+## Teknologi
+
+- **HTML5**: Untuk struktur halaman.
+- **CSS3**: Untuk styling dan tata letak.
+- **JavaScript**: Untuk interaktivitas.
+
+## Cara Menjalankan
+
+1. Clone repository ini ke komputer Anda:
+
+```bash
+git clone https://github.com/Iannn-vbeta/landingPage.git
+```
+
+2. Buka file `index.html` di browser Anda.
---
+++ js/script.js
@@ -0,0 +1,97 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+let allCards = [];
+let visibleCards = 4;
+let isAllVisible = false;
+
+document.getElementById("loadMoreBtn").addEventListener("click", toggleCards);
+
+function loadCards() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      allCards = data;
+      renderCards();
+    })
+    .catch((error) => {
+      console.error("Error loading JSON data:", error);
+    });
+}
+
+function renderCards() {
+  const cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = "";
+
+  for (let i = 0; i < visibleCards && i < allCards.length; i++) {
+    const cardData = allCards[i];
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const img = document.createElement("img");
+    img.src = cardData.image;
+    card.appendChild(img);
+
+    const title = document.createElement("h3");
+    title.textContent = cardData.title;
+    card.appendChild(title);
+
+    const description = document.createElement("p");
+    description.textContent = cardData.description;
+    card.appendChild(description);
+
+    const rate = document.createElement("div");
+    rate.classList.add("rate");
+    rate.textContent = `Coming soon`;
+    card.appendChild(rate);
+
+    cardContainer.appendChild(card);
+  }
+
+  const loadMoreBtn = document.getElementById("loadMoreBtn");
+  if (isAllVisible) {
+    loadMoreBtn.textContent = "Lebih Sedikit";
+  } else {
+    if (visibleCards >= allCards.length) {
+      loadMoreBtn.style.display = "none";
+    } else {
+      loadMoreBtn.style.display = "inline-block";
+      loadMoreBtn.textContent = "Selengkapnya";
+    }
+  }
+
+  const cardWrapper = document.querySelector(".card-wrapper");
+
+  if (isAllVisible) {
+    cardWrapper.style.paddingTop = "40px";
+    cardWrapper.style.paddingBottom = "40px";
+    cardWrapper.style.paddingTop = "20px";
+    cardWrapper.style.paddingBottom = "20px";
+  }
+}
+
+function toggleCards() {
+  if (isAllVisible) {
+    visibleCards = 4;
+    isAllVisible = false;
+  } else {
+    visibleCards = allCards.length;
+    isAllVisible = true;
+  }
+  renderCards();
+}
+
+loadCards();
